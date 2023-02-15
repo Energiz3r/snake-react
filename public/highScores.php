9a220d37bc6633ed61d77bbf459a5ab418fb7b70
Tim Eastwood <tim.eastwood@hotmail.com>
2023-02-15 15:29:09 +1100

    Update highScores.php

--- public/highScores.php
+++ public/highScores.php
@@ -2,30 +2,43 @@
 session_start();
 $data = json_decode(file_get_contents("php://input"), true);
 if(isset($data['newScore']) and isset($data['name'])){
-    echo("Setting new score ". $data['name'] . " " . $data['newScore']);
+    
+    $name = preg_replace("/[^a-zA-Z]/", "", $data['name']);
+    $newScore = preg_replace('/[^0-9]/', '', $data['newScore']);
+    if (strlen($name) < 1 or !is_numeric($newScore)) {
+        echo('invalid params!');
+        return;
+    }
+    
+    echo("Setting new score ". $name . " " . $newScore);
+    
     $content = file_get_contents('./highscores.txt');
     $scores = json_decode($content, true);
     $position = "none";
-    if ($data['newScore'] >= $scores["third"][1]) { $position = "third"; }
-    if ($data['newScore'] >= $scores["second"][1]) { $position = "second"; }
-    if ($data['newScore'] >= $scores["first"][1]) { $position = "first"; }
+    
+    if ($newScore >= $scores["third"][1]) { $position = "third"; }
+    if ($newScore >= $scores["second"][1]) { $position = "second"; }
+    if ($newScore >= $scores["first"][1]) { $position = "first"; }
     if ($position === "none") { return; }
+    
     if ($position === "first") {
         echo("new first place score");
         $scores["third"] = $scores["second"];
         $scores["second"] = $scores["first"];
-        $scores["first"] = array($data['name'], $_POST['newScore']);
+        $scores["first"] = array($name, $newScore);
     } elseif ($position === "second") {
         echo("new second place score");
         $scores["third"] = $scores["second"];
-        $scores["second"] = array($data['name'], $data['newScore']);
+        $scores["second"] = array($name, $newScore);
     } elseif ($position === "third") {
         echo("new third place score");
-        $scores["third"] = array($data['name'], $data['newScore']);
+        $scores["third"] = array($name, $newScore);
     }
+    
     $scoreJson = json_encode($scores);
     file_put_contents('./highscores.txt', $scoreJson);
     echo("Updated scores!");
+    
 } elseif (isset($_POST['defaultScores'])) {
     $defaultScores = array(
         "first" => array("Tim", 250),
@@ -36,11 +49,5 @@ if(isset($data['newScore']) and isset($data['name'])){
     file_put_contents('./highscores.txt', $scoreJson);
     echo("Defaulted the scores!");
 } else { 
-    $content = file_get_contents('./highscores.txt');
-    $scores = json_decode($content);
-    echo("<script>window.highScores = {");
-    foreach ($scores as $key => $value) {
-        echo($key . ":{name:'" . $value[0] . "',score:" . $value[1] . "},");
-    }
-    echo("}</script>");
+    echo("missing params!");
 }
